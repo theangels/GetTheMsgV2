8a45ca8b23544c9f66c03083caf7c86957a8ec87
郑楠 <zn373578008@outlook.com>
2016-06-17 16:59:26 +0800

    v2.11.3 程序可运行

--- app/src/main/java/ubibots/weatherbase/ui/ListTab.java
+++ app/src/main/java/ubibots/weatherbase/ui/ListTab.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +35,6 @@ public class ListTab {
         final List<String> data = new ArrayList<>();
         data.add("每时");
         data.add("每日");
-        data.add("活动推荐");
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(DisplayHistoryActivity.getActivity(), android.R.layout.simple_expandable_list_item_1, data);
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener((parent, view, position, id) -> {
@@ -50,10 +48,6 @@ public class ListTab {
                     dayVisible();
                     currentTab = 1;
                     break;
-                case "活动推荐":
-                    recommandVisable();
-                    currentTab = 2;
-                    break;
             }
         });
         hourVisible();
@@ -64,7 +58,6 @@ public class ListTab {
         if (HourView.getHourViewPager() != null) {
             HourView.getHourViewPager().setVisibility(View.VISIBLE);
             dayInvisible();
-            recommandInVisable();
         }
         if (HourView.getHourProgressBar().getVisibility() != View.GONE) {
             HourView.getHourProgressBar().setVisibility(View.VISIBLE);
@@ -87,7 +80,6 @@ public class ListTab {
         if (DayView.getDayViewPager() != null) {
             DayView.getDayViewPager().setVisibility(View.VISIBLE);
             hourInvisible();
-            recommandInVisable();
         }
         if (DayView.getDayProgressBar().getVisibility() != View.GONE) {
             DayView.getDayProgressBar().setVisibility(View.VISIBLE);
@@ -103,26 +95,6 @@ public class ListTab {
             if (DayView.getDayProgressBar().getVisibility() != View.GONE) {
                 DayView.getDayProgressBar().setVisibility(View.INVISIBLE);
             }
-        }
-    }
-
-    private void recommandVisable() {
-        if (RecommandView.getRecommandList() != null) {
-            RecommandView.getRecommandList().setVisibility(View.VISIBLE);
-            if (RecommandView.getRecommandList().getAdapter()==null) {
-                Toast.makeText(DisplayHistoryActivity.getContext(), "请耐心等待数据获取完毕", Toast.LENGTH_SHORT).show();
-            }
-            hourInvisible();
-            dayInvisible();
-        }
-        if (point != null) {
-            point.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    private void recommandInVisable() {
-        if (RecommandView.getRecommandList() != null) {
-            RecommandView.getRecommandList().setVisibility(View.INVISIBLE);
         }
     }
 }
